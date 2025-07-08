82806e6e723213debf9c892d0bba213b6ffbaae8
Solo-LA36 <fin82.82@yandex.ru>
2025-07-08 22:41:45 +0300

    Autotest in process

--- src/test/java/githubsearch/SelenideRepositorySearchTest.java
+++ src/test/java/githubsearch/SelenideRepositorySearchTest.java
@@ -1,27 +1,46 @@
 package githubsearch;
 
-import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideRepositorySearchTest {
 
 
-    @Test
-    void shouldFindJUnite5() {
+
+        @Test
+        void shouldFindJUnite5() {
 
         //открыть главную страницу github.com
-        open("https://github.com/");
-        $("title").click();
+        open("https://github.com");
 
 
-        // ввести в поле поиска selenide и нажать enter
-        //открыть страницу selenide в github
-        //перейти в раздел Wiki проекта selenide в github
-        //убедиться, что в списке страниц (Pages) есть страница SoftAssertions
-        // открыть страницу SoftAssertions, проверить, что внутри есть пример кода для JUnit5
+        // ввести в поле поиска selenide и и нажать enter
+            actions().sendKeys("s").perform();
+        $("#query-builder-test").setValue("selenide").pressEnter();
+
+            sleep(10000);
 
 
-    }
+        // перейти на страницу selenide
+
+
+        //перейти в раздел Wiki проекта selenide в github (Таб)
+
+
+        // проверка наличия текста SoftAssertions
+
+
+        //открытие страницы SoftAssertions
+
+
+        // проверить, что внутри есть пример кода для JUnit5
+
+
+
+        }
 }
