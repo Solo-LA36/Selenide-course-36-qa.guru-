c40773d4e4325bcbb84bd32dab84d19def9fe983
Solo-LA36 <fin82.82@yandex.ru>
2025-07-09 15:03:19 +0300

    Autotest done

--- src/test/java/githubsearch/SelenideRepositorySearchTest.java
+++ src/test/java/githubsearch/SelenideRepositorySearchTest.java
@@ -23,24 +23,27 @@ public class SelenideRepositorySearchTest {
             actions().sendKeys("s").perform();
         $("#query-builder-test").setValue("selenide").pressEnter();
 
-            sleep(10000);
 
-
-        // перейти на страницу selenide
-
+        // кликнуть на первый репозиторий из списка найденных
+            $$("[data-testid=results-list]").first().$("a").click();
 
         //перейти в раздел Wiki проекта selenide в github (Таб)
-
+            $("#wiki-tab").click();
 
         // проверка наличия текста SoftAssertions
+            $(byText("Show 3 more pages…")).click();
+            $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+            $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
 
 
         //открытие страницы SoftAssertions
+            $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+
 
 
         // проверить, что внутри есть пример кода для JUnit5
-
-
+            $(".markdown-body").shouldHave(text("3. Using JUnit5 extend test class:"));
+            $("#repo-content-pjax-container").shouldHave(text("jUnit5"));
 
         }
 }
